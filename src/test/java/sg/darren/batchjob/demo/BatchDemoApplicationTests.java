549a821510960a93d8ab915793fddb3346653af5
Xue Yu <xueyublue@gmail.com>
2022-09-30 17:13:22 +0800

    Initial Commit

---
+++ src/test/java/sg/darren/batchjob/demo/BatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.darren.batchjob.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
