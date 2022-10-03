9136664952a0f2297590658c394ce4e9a39c2a56
Xue Yu <xueyublue@gmail.com>
2022-10-03 14:49:55 +0800

    Added Read/Process/Write Job

--- src/test/java/sg/darren/batchjob/demo/_03_read_process_write/ItemReadProcessWriteTest.java
+++ src/test/java/sg/darren/batchjob/demo/_03_read_process_write/ItemReadProcessWriteTest.java
@@ -1,4 +1,4 @@
-package sg.darren.batchjob.demo._03_read_write;
+package sg.darren.batchjob.demo._03_read_process_write;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,8 +6,12 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
 import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +19,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Date;
 
-@SpringBootTest(classes = ItemReaderTest.TestConfig.class)
-class ItemReaderTest {
+@SpringBootTest(classes = ItemReadProcessWriteTest.TestConfig.class)
+class ItemReadProcessWriteTest {
 
     @Configuration
     @EnableBatchProcessing
@@ -41,16 +47,17 @@ class ItemReaderTest {
         @Bean
         public Job readWriteJob() {
             return jobBuilderFactory.get("readWriteJob")
-                    .start(readerStep())
+                    .start(step())
                     .build();
         }
 
         @Bean
-        public Step readerStep() {
-            return stepBuilderFactory.get("readJsonStep")
-                    .chunk(1)
+        public Step step() {
+            return stepBuilderFactory.get("readWriteJsonStep")
+                    .<Input, Output>chunk(1)
                     .reader(reader())
-                    .writer(System.out::println)
+                    .processor(processor())
+                    .writer(writer())
                     .build();
         }
 
@@ -63,9 +70,33 @@ class ItemReaderTest {
                 throw new IllegalArgumentException(ex);
             }
             return new JsonItemReaderBuilder<Input>()
+                    .name("jsonItemReader")
                     .jsonObjectReader(new JacksonJsonObjectReader<>(Input.class))
                     .resource(new FileSystemResource(file))
+                    .build();
+        }
+
+        @Bean
+        public ItemProcessor<Input, Output> processor() {
+            return new ItemProcessor<Input, Output>() {
+                @Override
+                public Output process(Input input) throws Exception {
+                    Output output = new Output();
+                    output.value = input.value;
+                    output.formattedValue = input.value == null ? "" : input.value.toUpperCase();
+                    output.date = new Date();
+                    return output;
+                }
+            };
+        }
+
+        @Bean
+        public ItemWriter<Output> writer() {
+            Resource outputResource = new FileSystemResource("output/output.json");
+            return new JsonFileItemWriterBuilder<Output>()
                     .name("jsonItemReader")
+                    .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+                    .resource(outputResource)
                     .build();
         }
 
@@ -75,6 +106,19 @@ class ItemReaderTest {
             @Override
             public String toString() {
                 return "Input{" +
+                        "value='" + value + '\'' +
+                        '}';
+            }
+        }
+
+        public static class Output {
+            public String value;
+            public String formattedValue;
+            public Date date;
+
+            @Override
+            public String toString() {
+                return "Output{" +
                         "value='" + value + '\'' +
                         '}';
             }
