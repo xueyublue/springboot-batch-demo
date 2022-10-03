b1721c1d6f043d899871669cb2b733c2b59d3ac5
Xue Yu <xueyublue@gmail.com>
2022-10-03 11:48:55 +0800

    Added HelloWorld batch job

--- src/main/java/sg/darren/batchjob/demo/BatchDemoApplication.java
+++ src/main/java/sg/darren/batchjob/demo/BatchDemoApplication.java
@@ -1,15 +1,29 @@
 package sg.darren.batchjob.demo;
 
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import sg.darren.batchjob.demo._02_hello_world.TriggerBatchJobService;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class BatchDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(BatchDemoApplication.class, args);
+
+        TriggerBatchJobService triggerBatchJobService = context.getBean(TriggerBatchJobService.class);
+        try {
+            triggerBatchJobService.runJob();
+        } catch (JobInstanceAlreadyCompleteException | JobExecutionAlreadyRunningException |
+                 JobParametersInvalidException | JobRestartException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
