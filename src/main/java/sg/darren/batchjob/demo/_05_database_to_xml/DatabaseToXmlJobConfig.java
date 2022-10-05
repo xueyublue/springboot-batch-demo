b07d4280f831c01d7077f01b90aa1a8db4d154d5
Xue Yu <xueyublue@gmail.com>
2022-10-05 16:12:37 +0800

    Added DatabaseToXmlJobConfig.databaseToXmlReaderRepository

--- src/main/java/sg/darren/batchjob/demo/_05_database_to_xml/DatabaseToXmlJobConfig.java
+++ src/main/java/sg/darren/batchjob/demo/_05_database_to_xml/DatabaseToXmlJobConfig.java
@@ -9,12 +9,14 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.xml.StaxEventItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.domain.Sort;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 import sg.darren.batchjob.demo._04_xml_to_database.XmlToDatabaseDto;
 
@@ -28,11 +30,13 @@ public class DatabaseToXmlJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final DataSource dataSource;
+    private final DatabaseToXmlRepository databaseToXmlRepository;
 
-    public DatabaseToXmlJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+    public DatabaseToXmlJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource, DatabaseToXmlRepository databaseToXmlRepository) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.dataSource = dataSource;
+        this.databaseToXmlRepository = databaseToXmlRepository;
     }
 
     @Bean
@@ -49,8 +53,8 @@ public class DatabaseToXmlJobConfig {
     @Qualifier("databaseToXmlStep")
     public Step databaseToXmlStep() {
         return stepBuilderFactory.get("xmlToDatabaseJobStep")
-                .<DatabaseToXmlDto, DatabaseToXmlDto>chunk(10)
-                .reader(databaseToXmlReader())
+                .<DatabaseToXmlEntity, DatabaseToXmlDto>chunk(10)
+                .reader(databaseToXmlReaderRepository())
                 .processor(databaseToXmlProcessor())
                 .writer(databaseToXmlWriter())
                 .build();
@@ -58,11 +62,11 @@ public class DatabaseToXmlJobConfig {
 
     @Bean
     @Qualifier("databaseToXmlReader")
-    public ItemReader<DatabaseToXmlDto> databaseToXmlReader() {
-        JdbcCursorItemReader<DatabaseToXmlDto> reader = new JdbcCursorItemReader<>();
+    public ItemReader<DatabaseToXmlEntity> databaseToXmlReaderJdbcCursor() {
+        JdbcCursorItemReader<DatabaseToXmlEntity> reader = new JdbcCursorItemReader<>();
         reader.setDataSource(dataSource);
         reader.setSql("SELECT id, first_name, last_name, email FROM database_to_xml");
-        reader.setRowMapper((rs, rowNum) -> DatabaseToXmlDto.builder()
+        reader.setRowMapper((rs, rowNum) -> DatabaseToXmlEntity.builder()
                 .id(rs.getInt("id"))
                 .firstName(rs.getString("first_name"))
                 .lastName(rs.getString("last_name"))
@@ -72,12 +76,28 @@ public class DatabaseToXmlJobConfig {
     }
 
     @Bean
+    @Qualifier("databaseToXmlReaderRepository")
+    public ItemReader<DatabaseToXmlEntity> databaseToXmlReaderRepository() {
+        Map<String, Sort.Direction> sorts = new HashMap<>();
+        sorts.put("id", Sort.Direction.DESC);
+        return new RepositoryItemReaderBuilder<DatabaseToXmlEntity>()
+                .name("databaseToXmlReaderRepository")
+                .repository(databaseToXmlRepository)
+                .methodName("findAll")
+                .sorts(sorts)
+                .build();
+    }
+
+    @Bean
     @Qualifier("databaseToXmlProcessor")
-    public ItemProcessor<DatabaseToXmlDto, DatabaseToXmlDto> databaseToXmlProcessor() {
-        return input -> {
-            input.setEmail(input.getEmail() != null ? input.getEmail().toUpperCase() : "");
-            return input;
-        };
+    public ItemProcessor<DatabaseToXmlEntity, DatabaseToXmlDto> databaseToXmlProcessor() {
+        return input ->
+                DatabaseToXmlDto.builder()
+                        .id(input.getId())
+                        .firstName(input.getFirstName())
+                        .lastName(input.getLastName())
+                        .email(input.getEmail() != null ? input.getEmail().toUpperCase() : "")
+                        .build();
     }
 
     @Bean
