b0cfbb0432cf165abcb3cb6b2818d44050fac7be
Xue Yu <xueyublue@gmail.com>
2022-10-05 11:57:49 +0800

    Added two more dummy jobs

---
+++ src/main/java/sg/darren/batchjob/demo/_04_xml_to_database/XmlToDatabaseJobConfig.java
@@ -0,0 +1,4 @@
+package sg.darren.batchjob.demo._04_xml_to_database;
+
+public class XmlToDatabaseJobConfig {
+}
---
+++ src/main/java/sg/darren/batchjob/demo/_05_database_to_xml/DatabaseToXmlJobConfig.java
@@ -0,0 +1,4 @@
+package sg.darren.batchjob.demo._05_database_to_xml;
+
+public class DatabaseToXmlJobConfig {
+}
